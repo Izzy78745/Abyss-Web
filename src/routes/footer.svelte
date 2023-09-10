eb02bcf168c54e0974bfae9b4e9b712525dbec72
Paxton Warin <main@paxtonwarin.com>
2023-09-10 19:23:18 -0400

    first commit

---
+++ src/routes/footer.svelte
@@ -0,0 +1,6 @@
+<footer>
+	<a href="https://discord.gg/goabyss" target="_blank">Discord</a>
+	<a href="https://github.com/Abyss-Services" target="_blank">GitHub</a>
+	<a href="https://intoabyss.org/apply" target="_blank">Apply for Staff</a>
+	<p>Copyright Abyss Services 2023</p>
+</footer>
